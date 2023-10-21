12eca1af43b6814e4452553f9f75862d5205df4d
osman yaycioglu <osman.yaycioglu@gmail.com>
2023-10-21 12:05:56 +0300

    day 4 2

--- src/main/java/com/assistt/training/advanced/java/customer/db/CustomerDBService.java
+++ src/main/java/com/assistt/training/advanced/java/customer/db/CustomerDBService.java
@@ -1,14 +1,15 @@
 package com.assistt.training.advanced.java.customer.db;
 
 import com.assistt.training.advanced.java.customer.services.models.Customer;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CustomerDBService {
 
-    @Autowired
-    private ICustomerDao customerDao;
+    private final ICustomerDao customerDao;
 
     public void insertCustomer(Customer customerParam){
         customerDao.save(customerParam);
--- src/main/java/com/assistt/training/advanced/java/customer/services/CustomerProvisionService.java
+++ src/main/java/com/assistt/training/advanced/java/customer/services/CustomerProvisionService.java
@@ -3,18 +3,19 @@ package com.assistt.training.advanced.java.customer.services;
 import com.assistt.training.advanced.java.customer.db.CustomerDBService;
 import com.assistt.training.advanced.java.customer.db.ICustomerDao;
 import com.assistt.training.advanced.java.customer.services.models.Customer;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.annotation.RequestScope;
 
 @Service
+@RequiredArgsConstructor
 public class CustomerProvisionService {
 
-    @Autowired
-    private CustomerDBService customerDBService;
+    private final CustomerDBService customerDBService;
 
     public void addCustomer(Customer customerParam){
-
+        customerDBService.insertCustomer(customerParam);
     }
 
 }
